b8d2b64300db52ba8f4caafba920c2b5794645de
Brian Stiner <Brian.Stiner@revature.net>
2022-01-31 11:16:22 -0800

    Merge_audio works but writes chinese to the txt

--- merge_audio.py
+++ merge_audio.py
@@ -1,13 +1,13 @@
 import moviepy.editor as mpy
 import os
-import datetime
+import time
 
 # slow, ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
 compression = "ultrafast"
 vcodec =   "libx264"
 videoquality = "24"
 
-txt_dir, music_dir = "description.txt", "music"
+txt_dir, music_dir = "description.txt", "music/Input"
 
 def merge_audio(description_dir, music_dir):
 
@@ -15,26 +15,26 @@ def merge_audio(description_dir, music_dir):
 
     # load description
     description = open(description_dir, "a+") 
+    description.write(u'\uFEFF'.encode('UTF-8'))
     description.write("\n")
     description.write("Tracklist  • • • ")
     description.write("\n")
 
 
     # load songs and add tracklist to description
-    videotime = 0
-    track_time = datetime.min
+    duration = 0
     for file in os.listdir(music_dir):
         filename = os.fsdecode(file)
         if filename.endswith(".mp3"):
             current_song = mpy.AudioFileClip(os.path.join(music_dir, filename))
             songs.append(current_song)
-            description.write(track_time.strftime("%H:%M:%S")+" • "+filename.split('.')[0]+"\n")
-            videotime += current_song.duration
-            track_time = datetime.fromtimestamp(videotime)
+            result = time.gmtime(duration)
+            description.write(f'{result.tm_hour}:{result.tm_min}:{result.tm_sec} • {filename.split(".")[0]}')
+            duration += current_song.duration
 
     # merge audio
     audio = mpy.concatenate_audioclips(songs)
-    audio.write_audiofile("audio.mp3")
+    audio.write_audiofile("music/Output/audio.mp3", codec="libmp3lame", bitrate="320k", ffmpeg_params=["-q:a", "5"])
 
 if __name__ == '__main__':
     merge_audio(txt_dir, music_dir)
